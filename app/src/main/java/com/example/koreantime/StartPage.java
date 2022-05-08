44972ac1fd628f857e027160f5a0374e919b9aa6
JongWon <3338761@naver.com>
2022-05-08 20:27:13 +0900

    임시 저장

--- app/src/main/java/com/example/koreantime/StartPage.java
+++ app/src/main/java/com/example/koreantime/StartPage.java
@@ -1,18 +1,28 @@
 package com.example.koreantime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class StartPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_page);
 
@@ -21,6 +31,8 @@ public class StartPage extends AppCompatActivity {
         TextView findPw = findViewById(R.id.findPw);
         TextView signUp = findViewById(R.id.signUp);
         TextView logIn = findViewById(R.id.login);
+
+        Button btn = findViewById(R.id.DB_practice);
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +58,28 @@ public class StartPage extends AppCompatActivity {
             }
         });
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                db.collection("user")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                if (task.isSuccessful()){
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Toast.makeText(StartPage.this, document.getData().values().toString(), Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Toast.makeText(StartPage.this, "에러", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
 
 
     }
