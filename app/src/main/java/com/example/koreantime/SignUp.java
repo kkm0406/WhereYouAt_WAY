76b41125822ec20b6c026ceb40acc671360986f7
MINSU <ancx1234@naver.com>
2022-05-14 17:16:37 +0900

    회원가입 수정, img 삭제

--- app/src/main/java/com/example/koreantime/SignUp.java
+++ app/src/main/java/com/example/koreantime/SignUp.java
@@ -20,7 +20,6 @@ import android.widget.*;
 public class SignUp extends AppCompatActivity {
 
     final String TAG = getClass().getSimpleName();
-    Dialog imgDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +27,10 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         EditText name = findViewById(R.id.name);
-        EditText id = findViewById(R.id.id);
+        EditText email = findViewById(R.id.email);
         EditText pw = findViewById(R.id.pw);
         EditText address = findViewById(R.id.address);
-        EditText tel = findViewById(R.id.tel);
-        TextView getImg = findViewById(R.id.getImg);
         TextView signUp = findViewById(R.id.signUp);
-
-        imgDialog = new Dialog(SignUp.this);
-        imgDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        imgDialog.setContentView(R.layout.get_img_dialog);
-
-        getImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showImgDialog();
-            }
-        });
 
 
         signUp.setOnClickListener(new View.OnClickListener() {
@@ -52,13 +38,13 @@ public class SignUp extends AppCompatActivity {
             public void onClick(View view) {
                 boolean signFlag = true;
                 if (name.getText().toString().length() == 0) {
-                    name.setHint("이름을 입력하세요");
+                    name.setHint("닉네임을 입력하세요");
                     name.requestFocus();
                     signFlag = false;
                 }
-                if (id.getText().toString().length() == 0) {
-                    id.setHint("ID를 입력하세요");
-                    id.requestFocus();
+                if (email.getText().toString().length() == 0) {
+                    email.setHint("이메일을 입력하세요");
+                    email.requestFocus();
                     signFlag = false;
                 }
                 if (pw.getText().toString().length() == 0) {
@@ -71,11 +57,6 @@ public class SignUp extends AppCompatActivity {
                     address.requestFocus();
                     signFlag = false;
                 }
-                if (tel.getText().toString().length() == 0) {
-                    tel.setHint("전화번호를 입력하세요");
-                    tel.requestFocus();
-                    signFlag = false;
-                }
                 if (signFlag) {
                     Toast.makeText(SignUp.this, "db에 저장해야됨", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(SignUp.this, StartPage.class);
@@ -84,34 +65,4 @@ public class SignUp extends AppCompatActivity {
             }
         });
     }
-
-
-    public void showImgDialog() {
-            imgDialog.show();
-
-            Button cancel = imgDialog.findViewById(R.id.cancle);
-            cancel.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    imgDialog.dismiss();
-                }
-            });
-
-            Button fromCamera = imgDialog.findViewById(R.id.fromCamera);
-            fromCamera.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(SignUp.this, "카메라 연결", Toast.LENGTH_SHORT).show();
-                }
-            });
-
-            Button fromFile = imgDialog.findViewById(R.id.fromFile);
-            fromFile.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(SignUp.this, "파일 저장소 연결", Toast.LENGTH_SHORT).show();
-
-                }
-            });
-        }
-    }
+}
